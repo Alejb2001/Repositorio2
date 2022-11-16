44361ef245d736e79a6ee4a54dd59061179c2b20
alejb <alejb@DESKTOP-18SLBL5>
2022-11-16 11:38:30 -0700

    Prueba num 1

---
+++ src/ejemplogit2/EjemploGit2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplogit2;
+
+/**
+ *
+ * @author alejb
+ */
+public class EjemploGit2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Los integrantes del equipo son Alejandro Burciaga, Jose Chavarria, Armando Alvidrez y Juan Pizarro");
+    }
+    
+}
